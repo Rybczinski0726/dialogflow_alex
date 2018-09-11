9febd9b0eb664009938b603efa63a6f01aa8d5cb
Yang Seung Chul <skyskai@naver.com>
2018-09-11 22:39:13 +0900

    fixed to POST

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ var app = express();
 app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/webhook',function(request, response) {
+app.post('/webhook',function(request, response) {
   const agent = new WebhookClient({ request, response });
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));

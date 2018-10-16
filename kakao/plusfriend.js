83203fcc0579ddc0a0abf35b60e2378acf935135
Yang Seung Chul <skyskai@naver.com>
2018-10-16 23:02:02 +0900

    kakao plusfriend feature

--- kakao/plusfriend.js
+++ kakao/plusfriend.js
@@ -1,8 +1,7 @@
 'use strict';
-const express = require('express');
-// var apiai = require("../module/apiai");
 var apiai = require("apiai");
-var webhook = express.Router();
+const express = require('express');
+var router = express.Router();
 
 //10. dialogflow의 프로젝트에 있는 client용 키
 var chatapp = apiai("a26c7c45ff4b41109fe17cb0d6628954", {
@@ -16,7 +15,7 @@ var oResponse = {
 var options = {
     sessionId: 'kakaotalk'
 };
-webhook.get('/keyboard',function(request,response){
+router.get('/keyboard',function(request,response){
   const menu = {
      type: "text",
 	 content:"Hello world"
@@ -24,7 +23,7 @@ webhook.get('/keyboard',function(request,response){
  response.json(menu);
 });
 //request.body.content
-webhook.post('/message',function(request,response){
+router.post('/message',function(request,response){
   let sUser_key = decodeURIComponent(request.body.user_key); // user's key
   let sType = decodeURIComponent(request.body.type); // message type
   let sContent = decodeURIComponent(request.body.content); // user's message
@@ -34,7 +33,7 @@ webhook.post('/message',function(request,response){
   var request = chatapp.textRequest(sContent, options);
   //test
   oResponse.message.text=sContent;
-     response.json(oResponse);
+     
     request.on('response', function(res) {
      // console.log(res);
      oResponse.message.text=res.result.fulfillment.speech;
@@ -49,4 +48,4 @@ request.on('error', function(error) {
 request.end();
 });
 
-exports.module = webhook;
+module.exports = router;
